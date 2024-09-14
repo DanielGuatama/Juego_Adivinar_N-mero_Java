7cb4705a3676e9fee47383985f9b46940599496d
Daniel Guatama <edaniel.guatama@gmail.com>
2024-09-13 19:04:47 -0500

    inicio del juego

---
+++ src/JuegoAdivinarNúmero.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class JuegoAdivinarNúmero {
+    public static void main(String[] args) {
+        Scanner numero = new Scanner(System.in);
+        int numeroGenerado = new Random().nextInt(100); // Genera un número aleatorio entre 0 y 100
+        int intentos = 0;
+
+
+
+
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Desarrollado por Daniel Guatama Estudiante G7 One Next Education");
+    }
+}
