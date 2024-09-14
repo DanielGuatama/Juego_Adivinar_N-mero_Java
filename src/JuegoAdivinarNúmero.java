0d8ef808c3692e092d8afa0ffaac5b7306a97f85
Daniel Guatama <edaniel.guatama@gmail.com>
2024-09-13 20:58:23 -0500

    final

--- src/JuegoAdivinarNúmero.java
+++ src/JuegoAdivinarNúmero.java
@@ -3,13 +3,28 @@ import java.util.Scanner;
 
 public class JuegoAdivinarNúmero {
     public static void main(String[] args) {
-        Scanner numero = new Scanner(System.in);
-        int numeroGenerado = new Random().nextInt(100); // Genera un número aleatorio entre 0 y 100
-        int intentos = 0;
 
+        Scanner teclado = new Scanner(System.in);
+        int generarNumeroAleatorio = new Random().nextInt(100);
+        int intentosMaximos = 5;
 
+        for (int intentos = 1; intentos <= intentosMaximos; intentos ++) {
+            System.out.println("Intento " + intentos + " Digite un número de 1 a 100");
+            int numeroDelusuario = teclado.nextInt();
 
+            if (numeroDelusuario == generarNumeroAleatorio) {
+                System.out.println("Haz acertado, ¡Ganaste!");
+                break;
+            } else if (numeroDelusuario < generarNumeroAleatorio) {
+                System.out.println("El número es mayor que " + numeroDelusuario);
+            } else {
+                System.out.println("El número es menor que " + numeroDelusuario);
+            }
 
-
+            if (intentos == intentosMaximos){
+                System.out.println("Lo siento no haz acertado, el número era " + generarNumeroAleatorio );
+            }
+        }
+        teclado.close();
     }
 }
